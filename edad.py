8fdc748deef27d69b30216bec6342fc20f60cec0
Yeiden2002 <yeiden2002@gmail.com>
2023-07-20 10:11:49 -0600

    practica edad

---
+++ edad.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+nombre = input("ingresa tu nombre")
+edad = int(input("ingresa tu edad"))
+if(edad>=18):
+    print(nombre,"eres mayor edad")
+else:
+    print(f"{nombre}eres mayor de edad")
+    
